9b40b942e9fd41a9ab765eb302343cce83b921af
DeFlesz <46074830+DeFlesz@users.noreply.github.com>
2020-11-05 01:04:18 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,8 +9,8 @@ int main() {
     Cetris map;
     srand(time(NULL));
     map.init();
-        map.update();
-        map.blocks.summonShape(rand() % 7 + 1);
+    map.update();
+    map.blocks.summonShape(rand() % 7 + 1);
     cout << map.blocks.parts[1][1];
 
     return 0;
